f63b66954f0806c6ad39569bba195fb7540a90b1
veryCreativeNameIndeed <104387753+veryCreativeNameIndeed@users.noreply.github.com>
2022-04-29 12:05:14 +0200

    adding FieldStates

---
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/FieldStates.java
@@ -0,0 +1,14 @@
+public class FieldStates{
+    public final int GROUND = 0;
+    public final int PLAYER_HEAD_OWN = 101;
+    public final int PLAYER_BODY_OWN = 102;
+    //public final int PLAYER_TAIL_OWN = 102;
+
+    public final int PLAYER_HEAD_OTHER = 201;
+    public final int PLAYER_BODY_OTHER = 202;
+    //public final int PLAYER_TAIL_OTHER = 202;
+    
+    public final int ITEM_APPLE = 301;
+
+    private FieldStates() {}
+}
