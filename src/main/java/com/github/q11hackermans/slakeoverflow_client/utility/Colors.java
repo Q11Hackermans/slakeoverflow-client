3518f5ab97a4eb41ccbc69988866eb26f7a5f3f0
veryCreativeNameIndeed <104387753+veryCreativeNameIndeed@users.noreply.github.com>
2022-04-29 12:18:32 +0200

    Update Colors.java

--- src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/Colors.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/Colors.java
@@ -7,20 +7,42 @@ import java.awt.Color;
 public class Colors {
     public static Color ground1 = Color.decode("#4aa832");
     public static Color ground2 = Color.decode("#356e26");
-    public static Color black = Color.decode("#000");
     
     public static Color playerBodyOwn = Color.decode("#4147e8");
-    public static Color playerHeadOwn = Color.decode("#4147e8");
-    public static Color playerTailOwn = Color.decode("#4147e8");
-
-    // red basicially
+    public static Color playerHeadOwn = Color.decode("#5459e3");
+    
+    public static Color playerBodyOther = Color.decode("#44455e");
+    public static Color playerHeadOther = Color.decode("#545573");
+    
+    public static Color itemApple = Color.decode("#4147e8");
+    
+    
+    //other
+    public static Color black = Color.decode("#000");
     public static Color debug = Color.RED;
 
     public static Color getColorFromCode(int code) {
         switch (code) {
-            case 1:
+            // color codes for own player | 100-199
+            case 101:
+                return playerHeadOwn;
+            case 102:
                 return playerBodyOwn;
-        }
+        
+
+            // color codes for other player(s) | 200-299
+            case 201:
+                return playerHeadOther;
+            case 202:
+                return playerBodyOther;
+        
+
+            //items eg. apples | 300-399
+            case 301:
+                return playerHeadOther;
+            
+            
+            }
         return debug;
     }
 
