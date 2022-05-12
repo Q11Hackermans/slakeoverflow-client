8e5d9dc6ba76a8be434dc665103c5678790601ad
Joshua <joshua-koeck@gmx.de>
2022-05-12 17:25:05 +0200

    cleanup 1/unlimited

---
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/Logger.java
@@ -0,0 +1,12 @@
+package com.github.q11hackermans.slakeoverflow_client.utility;
+
+public class Logger {
+
+    public static void error(String message) {
+        System.err.println("ERROR | " + message);
+    }
+
+    public static void info(String message) {
+        System.out.println("INFO | " + message);
+    }
+}
