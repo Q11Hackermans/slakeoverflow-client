929dcebe01a832457f30939e50f2745159b0bb76
jandie1505 <jande1234567890@gmail.com>
2022-05-12 20:56:36 +0200

    eventlistener

---
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/listeners/EventListener.java
@@ -0,0 +1,39 @@
+package com.github.q11hackermans.slakeoverflow_client.listeners;
+
+import net.jandie1505.connectionmanager.CMListenerAdapter;
+import net.jandie1505.connectionmanager.events.CMClientClosedEvent;
+import net.jandie1505.connectionmanager.events.CMClientCreatedEvent;
+import net.jandie1505.connectionmanager.utilities.dataiostreamhandler.events.DataIOUTFReceivedEvent;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class EventListener extends CMListenerAdapter {
+    // CLIENT EVENTS
+    @Override
+    public void onClientCreated(CMClientCreatedEvent event) {
+        // WAS PASSIEREN SOLL WENN DIE VERBINDUNG ERFOLGREICH WAR SCHREIBST DU HIER REIN
+        event.getClient(); // SO BEKOMMST DU DEN CLIENT
+    }
+
+    @Override
+    public void onClientClosed(CMClientClosedEvent event) {
+        // WAS PASSIEREN SOLL WENN DIE VERBINDUNG ABBRICHT SCHREIBST DU HIER REIN
+        event.getClient(); // SO BEKOMMST DU (MAL WIEDER) DEN CLIENT)
+        event.getReason(); // SO BEKOMMST DU DEN GRUND WARUM DIE VERBINDUNG GETRENNT WURDE
+    }
+
+    // DATA IO EVENTS
+    @Override
+    public void onUTFReceived(DataIOUTFReceivedEvent event) {
+        try {
+            JSONObject jsonObject = new JSONObject(event.getData());
+
+            // WENN VOM SERVER NE NACHRICHT KOMMT, BEKOMMST DU DIE HIER
+
+            jsonObject.getString("cmd"); // SO HOLST DU DIR DEN STRING CMD AUS DEM JSONOBJECT RAUS
+        } catch(JSONException e) {
+            System.out.println("Received data in wrong format. Disconnecting...");
+            event.getClient().close();
+        }
+    }
+}
