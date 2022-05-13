0dbd8ae27c56ff3eb6720f5ef5651d4355459a00
Shadow199815 <Shadow-15@web.de>
2022-05-13 12:31:09 +0200

    I tried my best in Login Panel

--- src/main/java/com/github/q11hackermans/slakeoverflow_client/panels/LoginPanel.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/panels/LoginPanel.java
@@ -1,9 +1,21 @@
 package com.github.q11hackermans.slakeoverflow_client.panels;
 
 import javax.swing.*;
+import java.awt.* ;
+import java.awt.event.* ;
 
 public class LoginPanel extends JPanel {
     public LoginPanel() {
+        this.setBackground(ground1);
 
+        // JButton mit Text "Drück mich" wird erstellt
+        JButton button = new JButton("Play");
+
+        // JButton wird dem Panel hinzugefügt
+        panel.add(button);
+
+        this.button.setBounds(50,100,100,30)
+
+        meinJFrame.setVisible(true);
     }
 }
