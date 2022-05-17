958ffd78ccb2cd8f0a164eab5e95e8f26db3c81b
Ersatzhase <102618574+Ersatzhase@users.noreply.github.com>
2022-05-17 11:29:22 +0200

    Update ActionCommands.java

--- src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/ActionCommands.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/ActionCommands.java
@@ -2,6 +2,7 @@ package com.github.q11hackermans.slakeoverflow_client.utility;
 
 public class ActionCommands {
     public static final String playButtonPressed = "playButtonPressed";
+    public static final String spectatorButtonPressed = "spectatorButtonPressed";
     public static final String connectButtonPressed = "connectButtonPressed";
     public static final String disconnectButtonPressed = "disconnectButtonPressed";
 }
