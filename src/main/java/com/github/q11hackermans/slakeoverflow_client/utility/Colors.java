97aee5f7fb033dc6c030072e3ee75b62fbe65cc7
Shadow199815 <Shadow-15@web.de>
2022-05-24 11:32:43 +0200

    Ich mag farben

--- src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/Colors.java
+++ src/main/java/com/github/q11hackermans/slakeoverflow_client/utility/Colors.java
@@ -11,12 +11,34 @@ public class Colors {
     public static final Color GROUND_2 = Color.decode("#017821");
     public static final Color BG = Color.decode("#356e26");
 
+    //apple colors
+    public static final Color apple_red = Color.decode("#e71f1f");
+    public static final Color apple_brown = Color.decode("#c35d2d");
+    public static final Color apple_white = Color.decode("#f0e7e2");
+
+    //potential bordercolors
+    public static final Color Border_dark_blue = Color.decode("#09205c");
+    public static final Color Border_dark_brown = Color.decode("#643620");
+    public static final Color Border_light_brown = Color.decode("#d7825a");
+    public static final Color Border_light_gray = Color.decode("#7a7c88");
+    public static final Color Border_orrange = Color.decode("#e09814");
+    public static final Color Border_pink = Color.decode("#de1bb3");
+    public static final Color Border_purple = Color.decode("#62355a");
+    public static final Color Border_turquoise = Color.decode("#14e5bd");
+    public static final Color Border_yellow = Color.decode("#ebdf5b");
+
+    //Body Colors
+    public static final Color Player_own = Color.decode("#5079e4");
+    public static final Color Player_enemy = Color.decode("#7a7c88");
+    public static final Color Player_eye_white = Color.decode("#ffffff");
+    public static final Color Player_eye_black = Color.decode("#000206");
 
     // CONSOLE COLORS
     public static final String CONSOLE_RESET = "\033[0m";
     public static final String CONSOLE_DEBUG = "\033[0;36m"; //light blue / cyan
     public static final String CONSOLE_ERROR = "\033[0;31m"; // red
     public static final String CONSOLE_INFO = CONSOLE_RESET; // no color
+
 
 
 }
